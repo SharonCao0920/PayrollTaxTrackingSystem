d21c92daa9e35e189cbe17b5874deaa3d1123dcd
Jade <amulet66666@gmail.com>
2024-02-19 12:14:23 -0800

    PDF comparison API implementation v1

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
+import os
 from functools import wraps
-from flask import Flask, redirect, render_template, session
+
 import pymongo
 from dotenv import load_dotenv
-import os 
+from flask import Flask, redirect, render_template, session
 
-app = Flask(__name__)  
+app = Flask(__name__)
 app.secret_key = "secret"
 
 load_dotenv()
 URI = os.getenv("DATABASE_URI")
 
-#database
+# database
 client = pymongo.MongoClient(URI)
 db = client.user_login_system
 
-#decorators
+
+# decorators
 def login_required(func):
     @wraps(func)
     def wrap(*args, **kwargs):
@@ -22,31 +24,36 @@ def login_required(func):
             return func(*args, **kwargs)
         else:
             return redirect('/')
-        
+
     return wrap
-#routes 
-from user import routes
+
+
+# routes
 
 @app.route('/')
 def home():
-    return render_template('login.html')   
+    return render_template('login.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')   
+    return render_template('about.html')
+
 
 @app.route('/services')
 def services():
-    return render_template('service.html') 
+    return render_template('service.html')
+
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html') 
+    return render_template('contact.html')
+
 
 @app.route('/homepage')
 def homepage():
-    return render_template('home.html') 
+    return render_template('home.html')
+
 
 # @app.route('/gologin')
 # def gologin():
@@ -54,17 +61,20 @@ def homepage():
 
 @app.route('/gosignup/')
 def goSignup():
-    return render_template('signup.html')   
+    return render_template('signup.html')
+
 
 @app.route('/dashboard/')
 @login_required
 def dashboard():
-    return render_template('dashboard.html')   
+    return render_template('dashboard.html')
+
 
 @app.route('/goforgetPass/')
 def goforgetPass():
-    return render_template('forgetpassword.html') 
+    return render_template('forgetpassword.html')
+
 
 @app.route('/goresetPass/')
 def goresetPass():
-    return render_template('resetpassword.html') 
+    return render_template('resetpassword.html')
